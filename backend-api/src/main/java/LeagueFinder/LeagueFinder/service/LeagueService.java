2d89d2c312c4220ebec198d9da6549597b0d067c
Amari <amariames443@gmail.com>
2026-07-22 08:27:50 -0400

    Add the backed league schedules

--- backend-api/src/main/java/LeagueFinder/LeagueFinder/service/LeagueService.java
+++ backend-api/src/main/java/LeagueFinder/LeagueFinder/service/LeagueService.java
@@ -21,6 +21,11 @@ public class LeagueService {
         return leagueRepository.findBySportIgnoreCase(sport);
     }
 
+    public league getLeagueById(Long id) {
+        return leagueRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("League not found."));
+    }
+
     public league createLeague(league league) {
         return leagueRepository.save(league);
     }
